9ed74b0cd554462d161d5bdbebf0a95fa5d801e2
정나래 <almighty62@naver.com>
2023-02-04 18:29:18 +0900

    [add] secret key와 input string을 가지고 xor 비트연산 하는 기능 추가

--- src/main.tsx
+++ src/main.tsx
@@ -37,6 +37,14 @@ function Main() {
     return secreyKeyBinary;
   };
 
+  const xorBitEnc = ({ input, secretKey }: XorEncStringT) => {
+    // 여기에서 체크해야될 사항, 시크릿키랑 아이템의 length가 달라질경우 처리하기
+    stringToBinary(input).forEach((item, index) => {
+      const bit = Number(item) ^ Number(makeSecretKey(secretKey)[index]);
+      console.log(bit, "enc 완료된", item, makeSecretKey(secretKey)[index]);
+    });
+  };
+
   /**
    * xor 비트 연산자 순서
    * 1. string 을 가지고 바이너리 파일로 만든다
@@ -44,7 +52,9 @@ function Main() {
    * 3. 바이너리 파일을 가지고 xor 비트 연산을 진행한다.
    */
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    xorBitEnc({ input: "test", secretKey: "bitt" });
+  }, []);
 
   return (
     <div style={{ padding: 20 }}>
